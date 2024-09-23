63b44c42fceb828f26cc3638cf55cd813070199c
mi2cha3 <michaelzheng223@gmail.com>
2024-09-23 18:20:50 +0900

    tukareta

--- C05_4/ex01/ft_recursive_factorial.c
+++ C05_4/ex01/ft_recursive_factorial.c
@@ -6,14 +6,14 @@
 /*   By: ktei <ktei@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/11 19:18:02 by ktei              #+#    #+#             */
-/*   Updated: 2024/09/11 19:46:52 by ktei             ###   ########.fr       */
+/*   Updated: 2024/09/21 21:59:19 by ktei             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_recursive_factorial(int nb)
 {
 	if (nb < 0)
-		return (0);
+		return (0); 
 	if (nb == 0)
 		return (1);
 	return (nb * ft_recursive_factorial(nb - 1));
